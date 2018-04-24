151d312fe2694ea21935f79e88413dded90c97a9
Christian Williams <williamschristian1521@gmail.com>
2018-04-24 03:23:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -146,8 +146,13 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+  if (e.which === LEFT_ARROW || e.which === RIGHT_ARROW)
+  {
+    e.stopPropagation()
+    e.preventDefault()
+  }
   if (e.which === LEFT_ARROW) moveDodgerLeft()
-  else if (e.which === RIGHT_ARROW) moveDodgerRight()
+  if (e.which === RIGHT_ARROW) moveDodgerRight()
 }
 
 
